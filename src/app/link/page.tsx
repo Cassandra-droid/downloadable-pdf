a3486dc491bf14c3fdad32a9cdfd16052bf9344c
Cassandra-droid <107626604+Cassandra-droid@users.noreply.github.com>
2024-06-27 09:46:58 +0300

    Update page.tsx

--- src/app/link/page.tsx
+++ src/app/link/page.tsx
@@ -1,36 +1,5 @@
-import Head from "next/head";
 
-const Link: React.FC = () => {
-  const pageTitle = "Your Specified Page Title";
-  const pageDescription = "Description of your specified page.";
-  const pageUrl = "https://downloadable-pdf.vercel.app/link";
-  const pageImageUrl = "https://downloadable-pdf.vercel.app/assets/table.png";
 
-  return (
-    <>
-      <Head>
-        <title>{pageTitle}</title>
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={pageTitle} />
-        <meta property="og:description" content={pageDescription} />
-        <meta property="og:image" content={pageImageUrl} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:url" content={pageUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={pageTitle} />
-        <meta name="twitter:description" content={pageDescription} />
-        <meta name="twitter:image" content={pageImageUrl} />
-      </Head>
-      <div>
-        <h1>{pageTitle}</h1>
-        <p>{pageDescription}</p>
-      </div>
-    </>
-  );
-};
-
-export default Link;
 
 
 
